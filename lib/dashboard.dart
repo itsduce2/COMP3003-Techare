705ba574fbfd3481e5d5ef5ff60afdb242660f51
Miles Butt <psymb18@nottingham.ac.uk>
2026-02-27 17:41:16 +0100

    feat: initial dashboard setup

---
+++ lib/dashboard.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _Dashboardtate();
+}
+
+class _Dashboardtate extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Welcome to Dashboard'),
+          ],
+        ),
+      ),
+    ); 
+  }
+}
