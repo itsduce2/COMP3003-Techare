777767f23ace3b276b1f0235be4c3b8e04ffcf6a
Miles Butt <psymb18@nottingham.ac.uk>
2026-03-06 15:27:50 +0100

    fix: old/ unused code

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -8,13 +8,6 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     
