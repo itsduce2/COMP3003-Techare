d0e45b57d74b0efe82a249317c1ae61848b0a9ca
Miles Butt <psymb18@nottingham.ac.uk>
2026-03-06 15:34:33 +0100

    ui: settings setup

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -3,14 +3,66 @@ import 'package:flutter/material.dart';
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: const Center(
-        child: Text('Settings Screen'),
+      // background colour
+      backgroundColor: Colors.blueGrey[50],
+      
+      body: SafeArea(
+        //SafeArea
+        child: SingleChildScrollView(
+          child: Column(
+            // vertical column layout
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            
+            // Header
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.all(24.0), // Padding
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Greeting column
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Hi, Amina 👋',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Here are your settings',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // Icons
+                  Row(
+                    children: const [
+                      Icon(Icons.notifications_none, size: 28),
+                      SizedBox(width: 16),
+                      Icon(Icons.account_circle, size: 40),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+              //padding
+              const SizedBox(height: 24),
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
