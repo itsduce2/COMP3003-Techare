f5e6e0cdf31aeb34ee77b6a245a898bfbad6c794
Miles Butt <psymb18@nottingham.ac.uk>
2026-03-09 20:54:32 +0100

    feat: device info service

---
+++ lib/device_info_service.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
+
+class DeviceInfoService {
+  static Future<Map<String, String>> getDeviceDetails() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+    // check if device is Android or iOS & return info
+    try {
+      //Android
+      if (Platform.isAndroid) {
+        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+        return {
+          'name': androidInfo.model,
+          'os': 'Android ${androidInfo.version.release}'
+        };
+      } 
+      
+      //iOS
+      else if (Platform.isIOS) {
+        IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+        return {
+          'name': iosInfo.name,
+          'os': iosInfo.systemVersion
+        };
+      }
+    } 
+    
+    //unknown device or device info error
+    catch (e) {
+      return {'name': 'Unknown Device', 'os': 'Unknown OS'};
+    }
+    
+    return {'name': 'Unknown Device', 'os': 'Unknown OS'};
+  }
+}
