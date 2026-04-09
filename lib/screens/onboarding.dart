d12abfead7feacc892073b95f3c5d381db641ae1
Miles Butt <psymb18@nottingham.ac.uk>
2026-04-09 13:14:40 +0200

    feat/navigation-added

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -8,10 +8,28 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  void _nextPage() {
+    _pageController.nextPage(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        onPageChanged: (int page) => setState(() => _currentPage = page),
+        children: [
+          Container(),
+        ],
+      ),
     );
   }
 }
